5185b07dc4b78202255e878b0b204a023934b472
Elsti97 <elsti.shehati@hotmail.it>
2022-10-27 18:19:40 +0200

    Aggiunto "function" palindromo

--- main.js
+++ main.js
@@ -3,14 +3,19 @@
 // Creare una funzione per capire se la parola inserita è palindroma
 
 let parola = prompt("Inseirisci una parola");
-let palindromo = "";
 
-for (let i = parola.length - 1; i >= 0; i--) {
+function ErPalindromo(){
+    let palindromo = '';
 
-    palindromo += parola[i];
+    for (let i = parola.length - 1; i >= 0; i--) {
+        palindromo += parola[i];
+    }
+    return palindromo;
+    
+
 }
 
-if (parola === palindromo) {
+if (parola == ErPalindromo()) {
     console.log(`la parola: ${parola} è palindroma`);
 } else {
     console.log(`la parola: ${parola} non è palindroma`);
